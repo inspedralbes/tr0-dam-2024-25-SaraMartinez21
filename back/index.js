b5446ab01e8101092ca897ae6211d7941318452d
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2024-10-10 15:51:21 +0200

    Refactor UUID module and update dependencies

--- back/index.js
+++ back/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const fs = require('fs'); 
 const app = express();
 const cors = require('cors');
-
+const { v4: uuidv4, validate } = require('uuid');
+//const { uuid } = require('uuidv4');
 const port = 3000;
 
 app.use(express.json());
@@ -16,14 +17,21 @@ fs.readFile('data.json', 'utf8', (err, data) => {
     }
     json = JSON.parse(data);
 });
+
+let partidas = {};
 app.get('/data', (req, res) => {
     res.json(json.preguntes);
 });
 
+function generateUid(){
+    if (uuid == undefined || !validate(uuid)) {
+        uid= uuidv4();
+    }
+}
+
 app.post('/addPreguntas', (req, res) => {
-    
     const lastId = json.preguntes.length > 0 ? json.preguntes[json.preguntes.length - 1].id : 0;
-    const newPregunta = { id: lastId + 1, ...req.body };
+    const newPregunta = { id: lastId + 1,uid ,...req.body };
 
     json.preguntes.push(newPregunta);
 
@@ -36,6 +44,54 @@ app.post('/addPreguntas', (req, res) => {
 
     res.json(newPregunta);
 });
+
+
+app.post('/preguntesPartida', (req, res) => {
+    const uid = req.body.uid;
+    const preguntes = json.preguntes;
+    const partida = {
+        uid: "",
+        preguntes: [],
+    };
+
+    for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * preguntes.length);
+        partida.preguntes.push(preguntes[randomIndex]);
+    }
+
+    partida.uid = uuidv4(); // Generar un UID único
+    partidas[partida.uid] = JSON.parse(JSON.stringify(partida));
+    console.log(partidas);
+    res.send(partida);
+});
+
+app.post('/submitRespuestas', (req, res) => {
+    const { token, respuestas } = req.body;
+
+    // Validación de los datos recibidos
+    if (!token || !Array.isArray(respuestas)) {
+        return res.status(400).send('Faltan datos o datos incorrectos');
+    }
+
+    // Buscar la partida correspondiente al token
+    const partida = partidas[token];
+    if (!partida) {
+        return res.status(404).send('Token no válido');
+    }
+
+    // Procesar las respuestas
+    let correctas = 0;
+    for (let i = 0; i < respuestas.length; i++) {
+        const pregunta = partida.preguntes[i];
+        if (pregunta.respostes[pregunta.resposta_correcta] === respuestas[i]) {
+            correctas++;
+        }
+    }
+
+    // Enviar el resultado al cliente
+    res.status(200).json({ correctas });
+});
+
 
 app.delete('/deletePregunta/:id', (req, res) => {
     const id = parseInt(req.params.id);
